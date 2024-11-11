d18a796dbebcf8eda762c36b8a1fa4a553e24200
William Klunder <82077867+Klundyy@users.noreply.github.com>
2024-11-11 11:42:41 -0800

    add button

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
 // todo
+import "./style.css";
+
+const app = document.querySelector<HTMLDivElement>("#app")!;
+
+const button = document.createElement("button");
+button.innerHTML = "click";
+app.append(button);
+button.addEventListener("click", () => {
+  alert("you clicked the button!");
+});
